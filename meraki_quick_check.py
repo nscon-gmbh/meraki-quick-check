431bfe7bc29414355b8e21654929803f9f28c94e
Daniel Kuhl <kuhl@nscon.de>
2022-08-04 23:04:33 +0200

    added option to choose org

--- meraki_quick_check.py
+++ meraki_quick_check.py
@@ -6,13 +6,41 @@ import tabulate
 import meraki
 
 # Set Meraki API Key via env variable or input if not set
-api_key = os.environ.get("NSCON_MERAKI_API_KEY") or input("\nEnter your Meraki API key: ")
+api_key = os.environ.get('YOUR_MERAKI_API_KEY') or input('\nEnter your Meraki API key: ')
 
 # Set Meraki Dashboard API call, send logs to log folder and omit log output on console
 dashboard = meraki.DashboardAPI(api_key, log_path="logs", print_console=False)
 
+# Get all organizations
+orgs = dashboard.organizations.getOrganizations()
+
+# Define table headers and values for organizations
+headers = ["No.", "Org Name", "Org ID"]
+table = []
+
+# If more than one organization then get all org data and print table
+i = 0
+if len(orgs) > 1:
+    for org in orgs:
+        print('\nYour API Key grants access to the following organizations:\n')
+        org_name = orgs[i]['name']
+        org_id = orgs[i]['id']
+        i += 1
+
+    # Add all organization data to table row and add row to table
+    row = [org_name, org_id]
+    table.append(row)
+
+    # Print table using the received organization data
+    print(tabulate.tabulate(table, headers=headers, tablefmt='fancy_grid', showindex=True))
+
+    org = input('\nChoose one organization ID from list: ')
+
+# Else choose the only organization available
+else:
+    org = orgs[0]
+
 # Get organization details and assign variables
-org = dashboard.organizations.getOrganizations()[0]
 org_id = org.get('id')
 org_name = org.get('name')
 
@@ -20,8 +48,8 @@ org_name = org.get('name')
 network_list = dashboard.organizations.getOrganizationNetworks(org_id)
 print(f'\nYour organization \"{org_name}\" with ID {org_id} includes the following networks:\n')
 
-# Define table headers and values
-headers=["No.", "Network Name", "Network ID", "Devices", "Clients last 24h", "Traffic last 24h"]
+# Define table headers and values for networks
+headers = ['No.', 'Network Name', 'Network ID', 'Devices', 'Clients last 24h', 'Traffic last 24h']
 table = []
 
 # Iterate through network_list and assign network variables
@@ -34,7 +62,7 @@ for network in network_list:
         network_id, total_pages='all', statuses='Online'))
     clients_offline = len(dashboard.networks.getNetworkClients(
         network_id, total_pages='all', statuses='Offline'))
-    clients = f"{clients_online} Online / {clients_offline} Offline"
+    clients = f'{clients_online} Online / {clients_offline} Offline'
 
     # Get devices of a network for each device model
     devices_list = dashboard.networks.getNetworkDevices(network_id)
@@ -65,21 +93,21 @@ for network in network_list:
     # Get network traffic for a network for the last 24h
     try:
         traffic_all = dashboard.networks.getNetworkTraffic(network_id, timespan=86400)
-        i=0
+        i = 0
         traffic_sum = 0
         for traffic in traffic_all:
             traffic = traffic_all[i]['recv'] + traffic_all[i]['sent']
             traffic_sum += traffic
-            i+=1
+            i += 1
         traffic_sum = f"{traffic_sum} bytes"
 
     # Set traffic sum to n/a in case traffic analysis is not activated on network
     except meraki.exceptions.APIError:
         traffic_sum = "n/a"
 
-    # Add all data to table row and add row to table
+    # Add all network data to table row and add row to table
     row = [network_name, network_id, devices, clients, traffic_sum]
     table.append(row)
 
-# Print table using the received data
+# Print table using the received network data
 print(tabulate.tabulate(table, headers=headers, tablefmt="fancy_grid", showindex=True))
