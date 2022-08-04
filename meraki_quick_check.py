91b8ba7e84747014e54bb3c61ffee6510990a8cb
Daniel Kuhl <kuhl@nscon.de>
2022-08-04 09:54:38 +0200

    added index

--- meraki_quick_check.py
+++ meraki_quick_check.py
@@ -15,22 +15,21 @@ network_list = dashboard.organizations.getOrganizationNetworks(org_id)
 
 # print(json.dumps(network_list,indent=4))
 
-print(f'\nYour organization \"{org_name}\" with ID {org_id} includes the following networks:')
+print(f'\nYour organization \"{org_name}\" with ID {org_id} includes the following networks:\n')
 
 # Define table headers and values
 headers=["No.", "Network Name", "Network ID"]
 table = []
-i=1
 
 for network in network_list:
     network_id = network['id']
     network_name = network['name']
     #print(f"\n- Name: {network_name}, ID: {network_id}")
     row = [network_name, network_id]
-    print(row)
+    #print(row)
     table.append(row)
 
-print(table)
-print(tabulate.tabulate(table, headers=headers, tablefmt="fancy_grid"))
+#print(table)
+print(tabulate.tabulate(table, headers=headers, tablefmt="fancy_grid", showindex=True))
 
 #print("\nPlease enter a number to get more health details of a network or press 0 for exit: ")
